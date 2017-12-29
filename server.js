9bb8c5bcdcda3c9c5556bafc12509a055476f4b2
Gregor Richardson <gregor.richardson@gmail.com>
2017-12-28 20:05:02 -0800

    starter code

--- server.js
+++ server.js
@@ -13,11 +13,5 @@ app.get('/', (request, response) => {
     console.log('This will show up in Node terminal window');
 });
 
-// Set up a route to send a file
-app.get('/house', (req, res) => {
-    res.sendFile('/public/modelHouse.html', {root: '.'});
-    // res.sendFile('modelHouse.html', {root: './public'}); // alternate path structure
-});
-  
 // Start the app so it listens for changes
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
