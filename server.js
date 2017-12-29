5f0459043e6191e8464ed8629c0acd3d93777ff4
Gregor Richardson <gregor.richardson@gmail.com>
2017-12-28 19:45:45 -0800

    setting up basic files

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express'); // load express from node_modules
+const app = express(); // instanciate express to use its functionality
+
+const PORT = process.env.PORT || 3000; // set a port. Look to environment variable if avaialble
+
+app.use(express.static('./public')); // tell server which directory to serve files from
+
+// Set up a route to send a message
+app.get('/', (request, response) => {
+    response.send('<h1>This will show up in the browser</h1>');
+    console.log('This will show up in Node terminal window');
+});
+
+// Set up a route to send a file
+app.get('/house', (req, res) => {
+    res.sendFile('/public/modelHouse.html', {root: '.'});
+    // res.sendFile('modelHouse.html', {root: './public'}); // alternate path structure
+});
+  
+// Start the app so it listens for changes
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
